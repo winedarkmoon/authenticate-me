a92b6da4ff63936cefc84775521283fbede54168
winedarkmoon <boxinghydra@proton.me>
2023-06-16 03:03:54 -0700

    complete section 2

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Api::SessionsController < ApplicationController
+  def show
+    if current_user
+      render json: { user: current_user }
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:email],
+      params[:password]
+    )
+
+    if @user
+      login!(@user)
+      render json: { user: @user.as_json }, status: :ok
+    else
+      render json: { errors: ["The provided credentials were invalid."] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: { message: "Logged out successfully" }, status: :ok
+    else
+      render json: { errors: ["Nobody signed in"] }, status: :not_found
+    end
+  end
+end

b979a79616fc36cd0eab09347d1e7ba92e157c40
winedarkmoon <boxinghydra@proton.me>
2023-06-16 06:38:12 -0700

    complete section 3

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::SessionsController < ApplicationController
   def show
     if current_user
-      render json: { user: current_user }
+      render "api/users/show"
     else
       render json: { user: nil }
     end
@@ -15,7 +15,7 @@ class Api::SessionsController < ApplicationController
 
     if @user
       login!(@user)
-      render json: { user: @user.as_json }, status: :ok
+      render "api/users/show"
     else
       render json: { errors: ["The provided credentials were invalid."] }, status: :unauthorized
     end
